bec19eabdadef2c2fc66516de03422cfd7bb664e
aks681 <akshayanand681@gmail.com>
2016-09-24 06:19:22 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,13 @@ var element=document.getElementById("main-text");
 element.onclick = function() {
   element.innerHTML='Nothing Much about me anymore except that I was born on 21/10/1998';  
 };
+ var img=document.getElementById("img");
+ var marginleft=0;
+ function moveright()
+ {
+     marginleft=marginleft+5;
+     img.style.marginLeft=marginleft+'px';
+ }
+ img.onclick = function() {
+     var interval = setInterval(moveright,50);
+ };
