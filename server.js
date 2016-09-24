e828ddf5b449ab14023c0319164ba1cc4e321d16
aks681 <akshayanand681@gmail.com>
2016-09-24 04:34:28 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article1', function(req,res){
 });
 
 app.get('/article2', function(req,res){
-    res.send('Article #2 will be displayed here.');
+    res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
 app.get('/article3', function(req,res){
-    res.send('Article #3 will be displayed here.');
+    res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
