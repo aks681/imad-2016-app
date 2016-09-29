2dc0b1cfec360689499baf87538375c9dba91112
aks681 <akshayanand681@gmail.com>
2016-09-29 19:32:02 +0630

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,16 @@ element.onclick = function() {
  };
  
  var button=document.getElementById('counter');
- var counter = 0;
  
  button.onclick = function() {
    var request = new XMLHttpRequest();
-   counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+   request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE ){
+         if(request.status === 200){
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+         }
+       }
+   };
  };
