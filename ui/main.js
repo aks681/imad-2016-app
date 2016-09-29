11b516df292fc70510492fc0aea770cbb9005e11
aks681 <akshayanand681@gmail.com>
2016-09-29 20:04:59 +0630

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,3 +30,17 @@ element.onclick = function() {
    request.open('GET','http://aks681.imad.hasura-app.io/counter',true);
    request.send(null);
  };
+ 
+ var nameInput = document.getElementById("name");
+ var name=nameInput.value;
+ var submit = document.getElementById("submitbtn");
+ submit.onclick = function() {
+    var names=['name1','name2','name3']; 
+    var list='';
+    for (var i=0; i< names.length; i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById("namelist");
+    ul.InnerHtml=list;
+ };
+ 
