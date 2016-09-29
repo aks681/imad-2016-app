880e8c0d9cb6c044faf1dc90b0a54013c71c3998
aks681 <akshayanand681@gmail.com>
2016-09-29 16:30:02 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.onclick = function() {
-  element.innerHTML='Nothing Much about me anymore except that I was born on 21/10/1998';  
+  element.innerHTML='I am interested in Computer and electronics...';  
 };
  var img=document.getElementById("img");
  var marginleft=0;
