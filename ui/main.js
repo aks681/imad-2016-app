d6f2fd2d160e08a4157ecc2770609fcf1df69a3e
aks681 <akshayanand681@gmail.com>
2016-09-30 14:02:40 +0630

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,12 +35,23 @@ element.onclick = function() {
  var nameinp=nameInput.value;
  var submit = document.getElementById('submitbtn');
  submit.onclick = function() {
-    var names = ['Akshay' , 'Me' , 'You' , 'Us']; 
-    var list='';
-    for (var i=0; i < names.length; i++){
-        list+= '<li>' + names[i] +  '</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+     var request = new XMLHttpRequest();
+      request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE ){
+         if(request.status === 200){
+              var names = request.responseText;
+              names=JSON.parse(names);
+              var list='';
+              for (var i=0; i < names.length; i++){
+               list+= '<li>' + names[i] +  '</li>';
+               } 
+               var ul=document.getElementById('namelist');
+               ul.innerHTML=list;
+          }
+       }
+      };
+    request.open('GET','http://aks681.imad.hasura-app.io/submitname?name='+nameinp,true);
+    request.send(null);
+   
  };
  
