1e3068ec0f4a9181f57f152e75eb610216652ecc
aks681 <akshayanand681@gmail.com>
2016-10-12 21:46:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,20 +101,6 @@ app.get('/ui/article.html/submitcomment',function(req,res){
    res.send(JSON.stringify(comments));
 });
 
-var comments=[];
-app.get('/article2/submitcomment',function(req,res){
-   var comment=req.query.comment;
-   comments.push(comment);
-   res.send(JSON.stringify(comments));
-});
-
-var comments=[];
-app.get('/article3/submitcomment',function(req,res){
-   var comment=req.query.comment;
-   comments.push(comment);
-   res.send(JSON.stringify(comments));
-});
-
 app.get('/:articlename', function(req,res){
     var articlename=req.params.articlename;
     res.send(articletemplate(articles[articlename]));
