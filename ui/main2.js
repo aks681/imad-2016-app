e6d7116e7883039ec82f16d8414252c0613e3c14
aks681 <akshayanand681@gmail.com>
2016-10-12 21:39:45 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,11 +1,11 @@
- var submit = document.getElementById('submitbtn');
+var submit = document.getElementById('submitbtn');
  submit.onclick = function() {
      var request = new XMLHttpRequest();
       request.onreadystatechange = function(){
        if(request.readyState === XMLHttpRequest.DONE ){
          if(request.status === 200){
-              var comments = request.responseText;
-              comments=JSON.parse(comments);
+              var names = request.responseText;
+              names=JSON.parse(names);
               var list='';
               for (var i=0; i < names.length; i++){
                list+= '<li>' + names[i] +  '</li>';
@@ -15,8 +15,9 @@
           }
        }
       };
-    var commentInput = document.getElementById('comments');
-    var comminp=commentInput.value;
-    request.open('GET','http://aks681.imad.hasura-app.io/ui/article.html/submitcomment?comment='+comminp,true);
+    var nameInput = document.getElementById('comments');
+    var nameinp=nameInput.value;
+    request.open('GET','http://aks681.imad.hasura-app.io/submitname?name='+nameinp,true);
     request.send(null);
-};
+   
+ };
