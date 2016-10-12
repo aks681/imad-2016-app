c6a3ee52be005356655acbceae72357811006082
aks681 <akshayanand681@gmail.com>
2016-10-12 19:59:28 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,23 +1,23 @@
- var submitII = document.getElementById('submitbtnii');
- submitII.onclick = function() {
+  var submit = document.getElementById('submitbtn');
+ submit.onclick = function() {
      var request = new XMLHttpRequest();
       request.onreadystatechange = function(){
        if(request.readyState === XMLHttpRequest.DONE ){
          if(request.status === 200){
-              var comments = request.responseText;
-              comments=JSON.parse(comments);
-              var text='';
-              for (var i=0; i < comments.length; i++){
-               text+= '<li>' + names[i] +  '</li>';
+              var names = request.responseText;
+              names=JSON.parse(names);
+              var list='';
+              for (var i=0; i < names.length; i++){
+               list+= '<li>' + names[i] +  '</li>';
                } 
                var ul=document.getElementById('commentlist');
-               ul.innerHTML=text;
+               ul.innerHTML=list;
           }
        }
       };
-    var inputcomment = document.getElementById('comments');
-    var comm=inputcomment.value;
-    request.open('GET','http://aks681.imad.hasura-app.io/ui/article.html/submitcomment?comment='+comm,true);
+    var nameInput = document.getElementById('comments');
+    var nameinp=nameInput.value;
+    request.open('GET','http://aks681.imad.hasura-app.io/submitname?name='+nameinp,true);
     request.send(null);
    
  };
