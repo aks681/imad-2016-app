11258185e5c3d6fdd11f7d7408e0f8be6b9ad578
aks681 <akshayanand681@gmail.com>
2016-10-12 21:44:24 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -8,7 +8,7 @@ var submit = document.getElementById('submitbtn');
               comments=JSON.parse(comments);
               var list='';
               for (var i=0; i < comments.length; i++){
-               list+= '<li>' + commentss[i] +  '</li>';
+               list+= '<li>' + comments[i] +  '</li>';
                } 
                var ul=document.getElementById('commentlist');
                ul.innerHTML=list;
