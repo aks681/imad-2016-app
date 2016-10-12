cd2984235ab09ebb29a2a15b5be7ef18de8e0d35
aks681 <akshayanand681@gmail.com>
2016-10-13 00:02:20 +0630

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ function articletemplate (data)
     </head>
     <body>
         <div class='container'> 
-            You've visited this page <span id="count">0</span> times..<br>
             <div>
               <a href='/'>${a}</a>
             </div>
