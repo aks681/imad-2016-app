99e9a15ed2602534b6bf259c97fc73d24c17ad0e
aks681 <akshayanand681@gmail.com>
2016-10-15 13:05:33 +0630

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,22 +3,22 @@ var element=document.getElementById("main-text");
 element.onclick = function() {
   element.innerHTML='I am interested in Computer and electronics...';  
 };
- var button=document.getElementById('counter');
+// var button=document.getElementById('counter');
  
- button.onclick = function() {
-   var request = new XMLHttpRequest();
-   request.onreadystatechange = function(){
-       if(request.readyState === XMLHttpRequest.DONE ){
-         if(request.status === 200){
-          var counter = request.responseText;
-          var span = document.getElementById('count');
-          span.innerHTML = counter.toString();
-         }
-       }
-   };
-   request.open('GET','http://aks681.imad.hasura-app.io/counter',true);
-   request.send(null);
- };
+// button.onclick = function() {
+//   var request = new XMLHttpRequest();
+//   request.onreadystatechange = function(){
+//       if(request.readyState === XMLHttpRequest.DONE ){
+//         if(request.status === 200){
+//          var counter = request.responseText;
+//          var span = document.getElementById('count');
+//          span.innerHTML = counter.toString();
+//         }
+//       }
+//   };
+//   request.open('GET','http://aks681.imad.hasura-app.io/counter',true);
+//   request.send(null);
+// };
  
  var submit = document.getElementById('submitbtn');
  submit.onclick = function() {
